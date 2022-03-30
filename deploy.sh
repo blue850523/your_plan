99288d8d361156c830fd1261041b6f1e73b1f4d5
jay.li <>
2022-03-30 16:15:59 +0800

    github pages 配置

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# 發生錯誤時執行終止指令
+set -e
+
+# 打包編譯
+yarn build
+
+# 移動至到打包後的dist目錄 
+cd dist
+
+# 因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+git init 
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# 以這個專案來講就要改成這樣以下這樣
+git push -f git@github.com:blue850523/your_plan.git master:gh-pages
+
+cd -
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/your_plan/'   // 對應GitHub項目名稱
 })
